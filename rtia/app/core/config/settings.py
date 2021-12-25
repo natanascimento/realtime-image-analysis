d546c76349192d53482f1c0d6381e8bc5ba5a0cd
Natan Nascimento <natan.nascimento@xpi.com.br>
2021-12-25 18:22:15 -0300

    feat: added settings method to get env vars

--- rtia/app/core/config/settings.py
+++ rtia/app/core/config/settings.py
@@ -1,20 +1,16 @@
 from os.path import abspath, dirname
 from os import environ
-from dotenv import load_dotenv, find_dotenv
+import dotenv
 
 
 class Settings: 
 
-    def __init__(self) -> None:
-        load_dotenv(find_dotenv())
+    dotenv.load_dotenv(dotenv.find_dotenv())
 
     ROOT_PATH = dirname(dirname(abspath(__file__)))
     
-    CAM_IP = environ['CAM_IP'].get()
-    CAM_PORT = environ['CAM_PORT'].get()
-    CAM_USER = environ['CAM_USER'].get()
-    CAM_PASS = environ['CAM_PASS'].get()
-    CAM_CHANNEL = environ['CAM_CHANNEL'].get()
-
-    def get_rtsp_url(self) -> str:
-        return f"rtsp://{self.CAM_IP}:{self.CAM_PORT}/user={self.CAM_USER}&password={self.CAM_PASS}&channel={self.CAM_CHANNEL}&stream=0.sdp?"
+    CAM_IP = environ.get('CAM_IP')
+    CAM_PORT = environ.get('CAM_PORT')
+    CAM_USER = environ.get('CAM_USER')
+    CAM_PASS = environ.get('CAM_PASS')
+    CAM_CHANNEL = environ.get('CAM_CHANNEL')
