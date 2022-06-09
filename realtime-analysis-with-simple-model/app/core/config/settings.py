60c57cd6f706b69b2d54c37bc747a2df39f77e0f
Natan Nascimento <natanascimentom@outlook.com>
2022-06-09 15:30:23 -0300

    chore: added simple model (#10)

---
+++ realtime-analysis-with-simple-model/app/core/config/settings.py
@@ -0,0 +1,12 @@
+from os.path import abspath, dirname, join
+
+
+class Settings: 
+
+    ROOT_PATH = dirname(dirname(dirname(dirname(abspath(__file__)))))
+    
+    PROJECT_PATH = dirname(dirname(dirname(abspath(__file__))))
+    
+    MODELS_PATH = join(PROJECT_PATH, "models")
+    
+    HAARCASCADE_FULL_BODY = join(MODELS_PATH, "haarcascade_fullbody.xml")
